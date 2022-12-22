5dda7889451102cc0b5f7330e36ada92713392c1
Pouya Rahimpour <mpouya.rhm9981@gmail.com>
2022-12-22 13:15:36 +0330

    modified project structure

---
+++ __init__.py
@@ -0,0 +1,2 @@
+__app_name__ = "Calculator"
+__version__  = "0.1.0"
---
+++ __main__.py
@@ -0,0 +1,6 @@
+from . import Stack
+
+if __name__ == __main__:
+    math_string = input()
+    math_string = "".join(math_string.split())
+    print(ReversePolishNotationConvertor(math_string))
