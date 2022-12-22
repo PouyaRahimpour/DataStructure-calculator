13df8400d01f5531d43ee9bafdc1546283d10b32
Pouya Rahimpour <mpouya.rhm9981@gmail.com>
2022-12-22 14:10:26 +0330

    changed __manin__.py

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -1,6 +1,6 @@
-import stack
+import calculator
 
-if __name__ == __main__:
+if __name__ == '__main__':
     math_string = input()
     math_string = "".join(math_string.split())
     print(ReversePolishNotationConvertor(math_string))
